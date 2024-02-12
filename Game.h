2830882e0030b7a9fde7a9289614c008f5953434
olegb <baranov.oleg@lll.kpi.ua>
2024-02-12 21:06:32 +0200

    add variables to Game

--- Game.h
+++ Game.h
@@ -27,9 +27,28 @@ public:
     void setWhosTurn(QString player);
 
     // public variables
-    QGraphicsScene *scene;
-    QVector<QVector<Cell *>> player1Map;
-    QVector<QVector<Cell *>> player2Map;
+    QGraphicsScene* scene;
+    QVector<QVector<Cell*>> player1Map;
+    QVector<QVector<Cell*>> player2Map;
+
+    QVector<QPair<Cell*, int>> hitedCell;
+
+
+
+    bool gameMode;
+    bool placeMode;
+
+    int zalpMode1;
+    int zalpMode2;
+
+    int numShipsPlayer1;
+    int numShipsPlayer2;
+
+    int numMinePlayer1;
+    int numMinePlayer2;
+
+
+    bool botMode;
 
 public slots:
     // public slots
@@ -48,16 +67,32 @@ private:
     int widthMapFPS;
     int heightMapFPS;
 
+    int max_length_ship;
+    int max_deck;
+    int temp_size_ship;
+    bool horizontal;
+    int currntShipsDeck;
+    int fuelNum;
+    int capitalNum;
+    int mineNum;
+    QString nameShip;
+
+
+    QVector<QVector<Cell*>> mapForPlaceShip;
+
+
     QString whosTurn_;
 
-    QGraphicsTextItem *textPlayer1;
-    QGraphicsTextItem *textPlayer2;
+    QGraphicsTextItem* textPlayer1;
+    QGraphicsTextItem* textPlayer2;
 
-    QGraphicsTextItem *wrongPlace;
-    QGraphicsTextItem *returnFire;
+    QGraphicsTextItem* wrongPlace;
+    QGraphicsTextItem* returnFire;
 
-    QVector<QGraphicsTextItem *> textLetters;
-    QVector<QGraphicsTextItem *> textNumbers;
+    QVector<QGraphicsTextItem*> textLetters;
+    QVector<QGraphicsTextItem*> textNumbers;
+
+    QVector<QGraphicsItem*> itemToDelete;
 };
 
 #endif // GAME_H
